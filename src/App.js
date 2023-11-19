9f69cd34455ace6cb5100772d69d0c8a09276e11
rachealmuchiri <rachealloveo6@gmail.com>
2023-11-19 19:45:08 +0300

    styled the navbar

--- src/App.js
+++ src/App.js
@@ -17,15 +17,6 @@ import "./App.css";
 const App = () => (
   <div>
     <Navbar />
-    {/* <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer /> */}
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
 
-const root = document.getElementById("root");
-const rootElement = (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
