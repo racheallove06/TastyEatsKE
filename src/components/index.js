38a8910d3c5d82e8c802bf6d4b4459040a09ff03
rachealmuchiri <rachealloveo6@gmail.com>
2023-11-19 18:52:37 +0300

    added the navbar

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import SubHeading from "./SubHeading/SubHeading";
+
+import MenuItem from "./MenuItem/MenuItem";
+import Navbar from "./Navbar/Navbar";
+
+export { SubHeading, MenuItem, Navbar };
--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = document.getElementById("root");
+const rootElement = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
